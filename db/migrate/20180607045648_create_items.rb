d07049e2777b1e093039b6573d52fc9da5255f00
HungPhan <hungpv@atmarkcafe.org>
2018-06-29 14:41:34 +0700

    generate controller

--- db/migrate/20180607045648_create_items.rb
+++
@@ -1,9 +0,0 @@
-class CreateItems < ActiveRecord::Migration[5.2]
-  def change
-    create_table :items do |t|
-      t.references :product, index: true
-      t.references :user, index: true
-      t.integer :amount, default: 1
-    end
-  end
-end
